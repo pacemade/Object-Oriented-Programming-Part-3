e629ef8a6e896f9001ff1a9f8ce4a2bb586f59ab
Danny Chang <dannychachang@gmail.com>
2017-12-20 13:23:45 -0500

    Finish name_check assignment

--- reinforcement.rb
+++ reinforcement.rb
@@ -1,12 +1,31 @@
+#
+# rando =*(1..10)
+#
+#   def odd_sum(array)
+#     total = 0
+#     array.each { |value|
+#       if value % 2 != 0
+#         total += value
+#       end
+#     }
+#     return total
+#   end
 
-rando =*(1..10)
 
-  def odd_sum(array)
-    total = 0
-    array.each { |value|
-      if value % 2 != 0
-        total += value
+  def name_check(name)
+    names = ['larry', 'sherry', 'gary']
+    names.each { |check|
+      if check == name
+        return puts "Hi #{check}!"
+      else
+        return puts "Who are you why are you here?"
       end
     }
-    return total
   end
+
+
+puts "Name please"
+
+name = gets.chomp
+
+name_check(name)
